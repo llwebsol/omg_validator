089757d3eebd63e6ac547e3a7f52127d7858c7c1
Nickolas Kenyeres <info@knickmedia.com>
2011-12-20 00:29:18 -0500

    Removed required files from modules since they were autoloaded.

--- lib/omg_validator/validators/alpha_numeric_validator.rb
+++ lib/omg_validator/validators/alpha_numeric_validator.rb
@@ -12,28 +12,3 @@ module OmgValidator
 end
 
 ActiveModel::Validations::AlphaNumericValidator = OmgValidator::Validators::AlphaNumericValidator
-
-# Checks if a string conforms to a valid alpha-numeric format (e.g. ABC123).
-class OmgAlphaNumericValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    reg = /^([a-z0-9])+$/i
-    unless reg.match(value)
-      record.errors[attribute] = "must contain only alpha-numeric characters"
-    end
-  end
-end
-
-module OmgValidator
-  module Valicators
-    class AlphaDashValidator < ActiveModel::EachValidator
-      def validate_each(record, attribute, value)
-        reg = /^([-a-z0-9_-])+$/i
-        unless reg.match(value)
-          record.errors[attribute] = "must contain only alpha-numeric characters and dashes"
-        end
-      end
-    end
-  end
-end
-
-ActiveModel::Validations::AlphaDashValidator = OmgValidator::Validators::AlphaDashValidator
