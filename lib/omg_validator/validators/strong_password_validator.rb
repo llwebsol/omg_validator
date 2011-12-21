55f0422ca001c83e75b6fd0e1cb23dd1a34ca7a3
Michael Mottola <mikemottola@gmail.com>
2011-12-21 01:21:14 -0500

    added strong password validator

---
+++ lib/omg_validator/validators/strong_password_validator.rb
@@ -0,0 +1,14 @@
+module OmgValidator
+  module Validators
+    class StrongPasswordValidator < ActiveModel::EachValidator
+      def validate_each(record, attribute, value)
+        reg = '/^\w*(?=\w*\d)(?=\w*[a-z])(?=\w*[A-Z])\w*$/'
+        unless reg.match(value)
+          record.errors[attribute] = "must contain at least a number, a lower case letter, and a upper case letter"
+        end
+      end
+    end
+  end
+end
+
+ActiveModel::Validations::StrongPasswordValidator = OmgValidator::Validators::StrongPasswordValidator
