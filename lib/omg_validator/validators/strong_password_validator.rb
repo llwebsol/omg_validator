9f17da7248b4dd36b2f54d0b15068b0167b2d03e
Michael Mottola <mikemottola@gmail.com>
2012-04-04 23:23:20 -0400

    updated regex for strong password validator

--- lib/omg_validator/validators/strong_password_validator.rb
+++ lib/omg_validator/validators/strong_password_validator.rb
@@ -1,15 +1,15 @@
 module OmgValidator
   module Validators
     # Checks whether password input is a strong password
-    # Must contain a least one number, one lower case letter and one upper case letter
+    # Must contain a least one number, one lower case letter and one upper case letter and must be at least 8 characters
     #
     # validates :password, strong_password: true
     class StrongPasswordValidator < ActiveModel::EachValidator
       def validate_each(record, attribute, value)
         return nil if value.nil?
-        reg = '/^\w*(?=\w*\d)(?=\w*[a-z])(?=\w*[A-Z])\w*$/'
+        reg = /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/
         unless reg.match(value)
-          record.errors[attribute] = "must contain at least a number, a lower case letter, and a upper case letter"
+          record.errors[attribute] = "must contain at least a number, a lower case letter, a upper case letter and must be at least 8 characters"
         end
       end
     end
