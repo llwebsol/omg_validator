05f2f4231c1ebfa85e2dbc3dedc4bcfe9a80f375
Michael Mottola <mikemottola@gmail.com>
2012-04-04 23:31:41 -0400

    updated regex and docs for phone number validator

--- lib/omg_validator/validators/phone_number_validator.rb
+++ lib/omg_validator/validators/phone_number_validator.rb
@@ -4,10 +4,13 @@ module OmgValidator
     # Currently only supports American and Canadian formats
     #
     # validates :phone, phone_number: true
+    #
+    # matches: 2342343423, 1-800-232-1232, 100-232-2322, 1905-232-2323,
+    # does not match: 3434343434343, 333333333, 905-232-23239, (111) 232-2322
     class PhoneNumberValidator < ActiveModel::EachValidator
       def validate_each(record, attribute, value)
         return nil if value.nil?
-        reg = /^(1?(-?\d{3})-?)?(\d{3})(-?\d{4})$/
+        reg = /^(\d[ -\.]?)?(\d{3}[ -\.]?)?\d{3}[ -\.]?\d{4}(x\d+)?$/
         unless reg.match(value)
           record.errors[attribute] = "must be a valid phone number"
         end
