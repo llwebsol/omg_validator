88aa7e5160cefe321fdd6202d1492c4b26aa4d07
Michael Mottola <mikemottola@gmail.com>
2012-04-04 23:17:59 -0400

    replaced regex for ip address validator

--- lib/omg_validator/validators/ip_address_validator.rb
+++ lib/omg_validator/validators/ip_address_validator.rb
@@ -6,7 +6,7 @@ module OmgValidator
     class IpAddressValidator < ActiveModel::EachValidator
       def validate_each(record, attribute, value)
         return nil if value.nil?
-        reg = '/\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/'
+        reg = /^\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$\b/
         unless reg.match(value)
           record.errors[attribute] = "must be a valid IP address"
         end
