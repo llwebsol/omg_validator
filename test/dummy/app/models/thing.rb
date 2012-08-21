e94e74534bd8c20d9b4930071ef7bc3deb34529b
Nickolas Kenyeres <nkenyeres@gmail.com>
2012-08-20 23:00:42 -0400

    Added attribute to Thing for testing AlphaNumericDashValidator class.

--- test/dummy/app/models/thing.rb
+++ test/dummy/app/models/thing.rb
@@ -3,11 +3,12 @@ require 'active_model'
 class Thing
   include ActiveModel::Validations
 
-  attr_accessor :alpha, :alpha_numeric, :alpha_dash, :zip_code, :postal_or_zip_code, :postal_code,
+  attr_accessor :alpha, :alpha_numeric, :alpha_numeric_dash, :alpha_dash, :zip_code, :postal_or_zip_code, :postal_code,
     :decimal, :integer, :numeric, :email, :ip_address, :url, :phone, :password, :date
 
   validates :alpha, alpha: true
   validates :alpha_numeric, alpha_numeric: true
+  validates :alpha_numeric_dash, alpha_numeric_dash: true
   validates :alpha_dash, alpha_dash: true
   validates :zip_code, zip_code: true
   validates :postal_or_zip_code, postal_or_zip_code: true
@@ -25,6 +26,7 @@ class Thing
   def initialize(options = {})
     @alpha = nil
     @alpha_numeric = nil
+    @alpha_numeric_dash = nil
     @alpha_dash = nil
     @zip_code = nil
     @postal_or_zip_code = nil
