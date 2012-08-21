c398fe2628ba0683107d5d8c1bd67cb2b7af841d
Nickolas Kenyeres <nkenyeres@gmail.com>
2012-08-20 23:01:37 -0400

    Updated the AlphaDashValidator so that it no longer accepts dashes and underscores as valid characters. This was because an AlphaNumericDashValidator was created for that use case.

--- lib/omg_validator/validators/alpha_dash_validator.rb
+++ lib/omg_validator/validators/alpha_dash_validator.rb
@@ -1,17 +1,14 @@
 module OmgValidator
   module Validators
-    # Checks whether input only contains alpha-numberic characters and dashes
+    # Checks whether input only contains alpha characters and dashes.
     #
     # validates :perma_link, alpha_dash: true
-    #
-    # matches: alpha-dash-23, ll2-ada, 343, Dfsdf, -,
-    # does not match: $10.00, sub.domain, id=343, (232)
     class AlphaDashValidator < ActiveModel::EachValidator
       def validate_each(record, attribute, value)
         return nil if value.blank?
-        reg = /^([-a-z0-9_-])+$/i
+        reg = /^([-a-z_-])+$/i
         unless reg.match(value)
-          record.errors[attribute] = "must contain only alpha-numeric characters and dashes"
+          record.errors[attribute] = "must contain only alpha characters and dashes"
         end
       end
     end
